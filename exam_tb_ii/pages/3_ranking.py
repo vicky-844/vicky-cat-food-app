bc5100d82b970544d1f897bf0b991f528f3f9996
vicky-844 <victoria.ballach844@gmail.com>
2025-01-28 12:22:33 +0100

    Delete exam_tb_ii directory

--- exam_tb_ii/pages/3_ranking.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Ranking",
-                   page_icon=":cat:")
-
-st.title("Ranking")
--- exam_tb_ii/pages/5_information.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Information",
-                   page_icon=":cat:")
-
-st.title("Information")

850c18d6000bc9a284ff5544a186c01970385bc9
vicky-844 <victoria.ballach844@gmail.com>
2025-02-28 20:38:53 +0100

    Delete pages directory

--- pages/2_search.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Search",
-                   page_icon=":cat:")
-
-st.title("Search")
--- pages/4_shopping_list.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Shopping List",
-                   page_icon=":cat:")
-
-st.title("Shopping List")
